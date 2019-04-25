5789990a7f35d13d284f1c6419590b1ce87abd43
Ajay Subramanian <ajaysubramanian110@gmail.com>
2019-04-25 16:15:50 +0530

    Changes to testbed and adds percentage optimal action plot

--- bandit/epsilon_greedy.py
+++ bandit/epsilon_greedy.py
@@ -7,22 +7,28 @@ import matplotlib.pyplot as plt
 sns.set()
 
 # global variables
-n_bandits = 2000 # == n_runs
+n_bandits = 2000
 n_arms = 10
-n_timesteps = 3000
-sample_period = 4
+n_timesteps = 1000
 
 # testbed
 mean_rewards = np.array([np.random.normal(size=n_arms) for i in range(n_bandits)])
 
-def get_actual_reward(bandit,arm):
-    return np.random.normal(loc=mean_rewards[bandit,arm],scale=0.1)
+def get_actual_reward_list(bandit):
+    reward_list = []
+    for a in range(n_arms):
+        reward_list.append(np.random.normal(loc=mean_rewards[bandit,a],scale=1.0))
+    
+    return np.array(reward_list)
 
 # greedy
 def greedy(epsilon=0, n_bandits=None, n_timesteps=None, Qt=None):
     rewards = []
+    optimal_action = []
+    Qt = np.zeros((n_bandits,n_arms))
     for i in range(n_bandits):
         bandit_rewards = []
+        bandit_optimal_action = []
         arm_count = np.zeros((n_arms))
         for j in range(n_timesteps):
             x = np.random.uniform()
@@ -31,31 +37,39 @@ def greedy(epsilon=0, n_bandits=None, n_timesteps=None, Qt=None):
             else:
                 at = np.random.randint(0,n_arms)
             arm_count[at] += 1
-            Rt = get_actual_reward(i,at)
+            actual_reward_list = get_actual_reward_list(i)
+            Rt = actual_reward_list[at]
 
-            if j%3 == 0:
-                bandit_rewards.append(Rt)
+            bandit_rewards.append(Rt)
+
+            if at == np.argmax(actual_reward_list):
+                bandit_optimal_action.append(1.0)
+            else:
+                bandit_optimal_action.append(0.0)
 
             Qt[i,at] = Qt[i,at] + (Rt - Qt[i,at])/(arm_count[at] + 1)
 
             if j%50 == 0:
                 print("Bandit: {} Timestep: {} Action: {} Reward: {}".format(i,j,at,Rt))
         rewards.append(bandit_rewards)
+        optimal_action.append(bandit_optimal_action)
         print()
 
-    return Qt, rewards
+    return Qt, rewards, optimal_action
 
 # run on varying epsilon values
-for e in [0.1,0.0]:
-    Qt = np.zeros((1,n_arms))
-    Qt, rewards_exp = greedy(epsilon=1,n_bandits=1,n_timesteps=sample_period,Qt=Qt)
+fig, (ax1,ax2) = plt.subplots(2,1,sharex=True)
 
-    Qt, rewards = greedy(epsilon=e,n_bandits=1,n_timesteps=n_timesteps,Qt=Qt)
+for e in [0.0,0.01,0.1]:
+    Qt, rewards, optimal_action = greedy(epsilon=e,n_bandits=n_bandits,n_timesteps=n_timesteps)
+    rewards = np.array(rewards)
+    optimal_action = np.array(optimal_action)
+    percentages = (np.sum(optimal_action,0) * 100)/optimal_action.shape[0]
+
     print(mean_rewards[0])
     print(Qt[0])
 
-    combined_rewards = rewards_exp[0] + rewards[0]
-    plt.plot(range(len(combined_rewards)),combined_rewards)
+    ax1.plot(range(len(np.mean(rewards,0))),np.mean(rewards,0))
+    ax2.plot(range(len(percentages)),percentages)
     
-plt.title("epsilon 0.1 vs 0.0 for sample_period = 4, n_timesteps=2000 for reward variance 0.1")
 plt.show()
