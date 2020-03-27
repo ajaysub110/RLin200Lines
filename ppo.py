e388b48e1c01fd21e0d6940e4946a0fb29af8d51
Ajay Subramanian <ajaysubramanian110@gmail.com>
2020-03-27 13:40:29 +0530

    code reformatted

--- ppo.py
+++ ppo.py
@@ -1,18 +1,27 @@
+"""
+Proximal Policy Optimization
+PyTorch implementation of Proximal Policy Optimization with clipping loss and
+deep networks for policy and value function parameters.
+"""
+
+# Imports
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as opt 
+import torch.optim as opt
 from torch.autograd import Variable
 from torch.distributions import Categorical
-import gym 
+import gym
 from torch.utils.tensorboard import SummaryWriter
 
-env = gym.make('CartPole-v1')
+# Create env and set env and torch seeds
+env = gym.make("CartPole-v1")
 env.seed(42)
 torch.manual_seed(42)
 print(torch.cuda.is_available())
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
+# Training hyperparameters
 lr_p = 0.001
 lr_v = 0.005
 gamma = 0.99
@@ -20,16 +29,18 @@ batch_size = 8
 epsilon = 0.2
 copy_interval = 20
 
+
+# Policy network
 class PolicyNet(nn.Module):
     def __init__(self):
         super(PolicyNet, self).__init__()
 
         self.state_space = env.observation_space.shape[0]
-        self.action_space = env.action_space.n 
+        self.action_space = env.action_space.n
 
-        self.fc1 = nn.Linear(self.state_space,24)
-        self.fc2 = nn.Linear(24,24)
-        self.fc3 = nn.Linear(24,self.action_space)
+        self.fc1 = nn.Linear(self.state_space, 24)
+        self.fc2 = nn.Linear(24, 24)
+        self.fc3 = nn.Linear(24, self.action_space)
 
         self.gamma = gamma
         self.policy_hist = Variable(torch.Tensor())
@@ -38,15 +49,12 @@ class PolicyNet(nn.Module):
 
     def forward(self, x):
         model = nn.Sequential(
-            self.fc1,
-            nn.ReLU(),
-            self.fc2,
-            nn.ReLU(),
-            self.fc3,
+            self.fc1, nn.ReLU(), self.fc2, nn.ReLU(), self.fc3,
             nn.Softmax(dim=-1)
         )
 
         return model(x)
+
 
 class ValueNet(nn.Module):
     def __init__(self):
@@ -54,66 +62,70 @@ class ValueNet(nn.Module):
 
         self.state_space = env.observation_space.shape[0]
 
-        self.fc1 = nn.Linear(self.state_space,24)
-        self.fc2 = nn.Linear(24,1)
+        self.fc1 = nn.Linear(self.state_space, 24)
+        self.fc2 = nn.Linear(24, 1)
 
         self.value_hist = Variable(torch.Tensor())
         self.loss_hist = Variable(torch.Tensor())
 
     def forward(self, x):
-        model = nn.Sequential(
-            self.fc1,
-            nn.ReLU(),
-            self.fc2
-        )
+        model = nn.Sequential(self.fc1, nn.ReLU(), self.fc2)
 
         return model(x)
 
+
+# Instantiate networks and optimizers
 policy_new, policy_old = PolicyNet().to(device), PolicyNet().to(device)
 policy_old.load_state_dict(policy_new.state_dict())
 value = ValueNet().to(device)
-optimizer_policy = opt.Adam(policy_new.parameters(),lr=lr_p)
+optimizer_policy = opt.Adam(policy_new.parameters(), lr=lr_p)
 optimizer_value = opt.Adam(value.parameters(), lr=lr_v)
-tb = True 
+tb = True
 if tb:
     writer = SummaryWriter()
 
+
+# stochastic policy
 def select_action(s):
     state = torch.from_numpy(s).type(torch.FloatTensor).to(device)
-    
+
     probs_old = policy_old(Variable(state))
     probs_new = policy_new(Variable(state))
     val = value(Variable(state))
     c = Categorical(probs=probs_old)
     action = c.sample()
 
-    policy_old.policy_hist = torch.cat([
-        policy_old.policy_hist, probs_old[action].unsqueeze(0)
-    ])
+    policy_old.policy_hist = torch.cat(
+        [policy_old.policy_hist, probs_old[action].unsqueeze(0)]
+    )
 
-    policy_new.policy_hist = torch.cat([
-        policy_new.policy_hist, probs_new[action].unsqueeze(0)
-    ])
+    policy_new.policy_hist = torch.cat(
+        [policy_new.policy_hist, probs_new[action].unsqueeze(0)]
+    )
 
     value.value_hist = torch.cat([value.value_hist, val])
 
-    return action 
+    return action
 
+
+# compute losses for current trajectories
 def get_traj_loss():
     R = 0
     returns = []
 
     for r in policy_old.traj_reward[::-1]:
         R = r + policy_old.gamma * R
-        returns.insert(0,R)
+        returns.insert(0, R)
 
     returns = torch.FloatTensor(returns).to(device)
-    #returns = (returns - returns.mean()) / (returns.std() + np.finfo(np.float32).eps)
     A = Variable(returns) - Variable(value.value_hist)
     ratio = torch.div(policy_new.policy_hist, policy_old.policy_hist)
-    clipping = torch.clamp(ratio, 1-epsilon, 1+epsilon).mul(A).to(device)
+    clipping = torch.clamp(ratio, 1 - epsilon, 1 + epsilon).mul(A).to(device)
 
-    loss_policy = torch.mean(torch.min(torch.mul(ratio, A), clipping)).mul(-1).unsqueeze(0)
+    loss_policy = (
+        torch.mean(torch.min(torch.mul(ratio, A), clipping)).mul(
+            -1).unsqueeze(0)
+    )
     loss_value = nn.MSELoss()(value.value_hist, Variable(returns)).unsqueeze(0)
 
     policy_new.loss_hist = torch.cat([policy_new.loss_hist, loss_policy])
@@ -124,6 +136,8 @@ def get_traj_loss():
     policy_new.policy_hist = Variable(torch.Tensor())
     value.value_hist = Variable(torch.Tensor())
 
+
+# network update after every batch of trajectories (end of episode)
 def update_policy(ep):
     loss_policy = torch.mean(policy_new.loss_hist)
     loss_value = torch.mean(value.loss_hist)
@@ -142,6 +156,7 @@ def update_policy(ep):
 
     policy_new.loss_hist = Variable(torch.Tensor())
     value.loss_hist = Variable(torch.Tensor())
+
 
 def main():
     for ep in range(1000):
@@ -170,6 +185,7 @@ def main():
 
         if ep % copy_interval == 0:
             policy_old.load_state_dict(policy_new.state_dict())
+
 
 main()
 if tb:
